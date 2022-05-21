ca48ad58222fe8ee1c6b6caf27204fba0b558f0b
xennia316 <xennia316@gmail.com>
2022-05-21 18:45:25 +0100

    model modified

--- Models/Hotel.model.js
+++ Models/Hotel.model.js
@@ -1,27 +1,32 @@
 const mongoose = require("mongoose");
 
-const HotelSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const HotelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    town: {
+      type: String,
+      required: true,
+    },
+    evaluation: {
+      type: Number,
+      required: true,
+    },
+    recommendation: {
+      type: Boolean,
+      required: true,
+    },
+    contact: {
+      type: Number,
+      required: true,
+    },
   },
-  town: {
-    type: String,
-    required: true,
-  },
-  evaluation: {
-    type: Number,
-    required: true,
-  },
-  recommendation: {
-    type: Boolean,
-    required: true,
-  },
-  contact: {
-    type: Number,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Hotels = mongoose.model("Hotels", HotelSchema);
 
