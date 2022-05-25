6dcab53a7f6131cf7cb7cb58df7d4ded3cf0b2cf
xennia316 <xennia316@gmail.com>
2022-05-25 11:22:30 +0100

    removed

--- Routes/add_room.js
+++
@@ -1,6 +0,0 @@
-app.post("/add-room", async (req, res) => {
-  const { size, price, number, type } = await req.body;
-  const newRoom = await new RoomModel({ size, price, number, type });
-  newRoom.save();
-  res.json({ message: "Room added", data: newRoom });
-});
