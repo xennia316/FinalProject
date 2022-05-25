8bbb103c694871c32db55caf2aea172d895c309f
xennia316 <xennia316@gmail.com>
2022-05-25 11:23:15 +0100

    server updated

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
-const HotelModel = require("./Models/Hotel.model");
-const RoomModel = require("./Models/Room.model");
-const Hotels = require("./Models/Hotel.model");
+const HotelRouter = require("./Routes/Hotel.router");
+const RoomRouter = require("./Routes/Room.router");
+
 require("dotenv").config();
 
 // Database connection
@@ -27,30 +27,8 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use(cors());
 
-app.post("/add-hotel", async (req, res) => {
-  const { name, town, evaluation, recommendation, contact } = await req.body;
-  const newHotel = await new HotelModel({
-    name,
-    town,
-    evaluation,
-    recommendation,
-    contact,
-  });
-  newHotel.save();
-  res.json({ message: "Your hotel has been created", data: newHotel });
-});
-
-// Searching a hotel by Name
-app.get("/find-hotel", async (req, res) => {
-  const hotelName = await req.body;
-
-  const Hotel = await Hotels.findOne({ hotelName: hotelName });
-  if (Hotel) {
-    res.json({ message: `Your hotel: ${hotelName}`, data: Hotel });
-  } else {
-    res.send(`Your hotel ${hotelName} could not be found`);
-  }
-});
+app.use("/api/hotel", HotelRouter);
+app.use("/api/room", RoomRouter);
 
 // Declaring port number
 const PORT = process.env.PORT || 5000;
