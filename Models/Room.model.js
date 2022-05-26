48d1675c7178d31f1ba3b03066af8520df2ef496
xennia316 <xennia316@gmail.com>
2022-05-26 19:06:15 +0100

    Room model revisited

--- Models/Room.model.js
+++ Models/Room.model.js
@@ -1,22 +1,27 @@
 const mongoose = require("mongoose");
 
-const RoomSchema = new mongoose.Schema({
-  category: {
-    type: String,
+const RoomSchema = new mongoose.Schema(
+  {
+    category: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    number: {
+      type: Number,
+      required: true,
+    },
+    imageURL: {
+      type: String,
+    },
+    hotelId: { type: mongoose.ObjectId, ref: "hotels" },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  number: {
-    type: Number,
-    required: true,
-  },
-  imageURL: {
-    type: String,
-  },
-  hotelId: { type: mongoose.ObjectId, ref: "hotels" },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Room = mongoose.model("room", RoomSchema);
 
