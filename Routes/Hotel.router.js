2f06610869181a4c909f5f59a7a25869e2faba67
xennia316 <xennia316@gmail.com>
2022-05-26 01:12:17 +0100

    new mwthods added

--- Routes/Hotel.router.js
+++ Routes/Hotel.router.js
@@ -39,4 +39,28 @@ router.post("/find-hoteltown", async (req, res) => {
   }
 });
 
+router.post("/update-hotel", async (req, res) => {
+  const { name, town, evaluation, recommendation, contact } = await req.body;
+  const updatedHotel = await Hotels.findOneAndUpdate({
+    name,
+    town,
+    evaluation,
+    recommendation,
+    contact,
+  });
+  updatedHotel.save();
+  res.json({ message: "Here's your updated hotel data", data: updatedHotel });
+});
+
+router.delete("/delete-hotel", async (req, res) => {
+  const { name } = await req.body;
+  const Hotel = await Hotels.findOneAndDelete({ name })
+    .then(() => {
+      res.send("Hotel successfully deleted");
+    })
+    .catch(() => {
+      res.send("Unable to delete this hotel");
+    });
+});
+
 module.exports = router;
