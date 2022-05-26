a03a202fd82c096a69cbe30e192101c9908036ca
xennia316 <xennia316@gmail.com>
2022-05-26 19:05:50 +0100

    new changes to hotel model

--- Models/Hotel.model.js
+++ Models/Hotel.model.js
@@ -1,28 +1,40 @@
 const mongoose = require("mongoose");
 
-const HotelSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const HotelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    town: {
+      type: String,
+      required: true,
+    },
+    evaluation: {
+      type: Number,
+      required: true,
+    },
+    recommendation: {
+      type: Boolean,
+      required: true,
+    },
+    contact: {
+      type: Number,
+      required: true,
+    },
+    ownerID: {
+      type: mongoose.ObjectId,
+      ref: "users",
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
   },
-  town: {
-    type: String,
-    required: true,
-  },
-  evaluation: {
-    type: Number,
-    required: true,
-  },
-  recommendation: {
-    type: Boolean,
-    required: true,
-  },
-  contact: {
-    type: Number,
-    required: true,
-  },
-});
-
+  {
+    timestamps: true,
+  }
+);
 const Hotels = mongoose.model("hotels", HotelSchema);
 
 module.exports = Hotels;
