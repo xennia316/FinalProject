243718d354b7f95fdcfde518b85afa053d55ba69
xennia316 <xennia316@gmail.com>
2022-05-26 01:12:30 +0100

    user routes in progress

---
+++ Routes/User.router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router;
+
+const UserModel = require("../Models/User.model");
+
+router.post("/sign-up", async (req, res) => {
+  const { name, email, town, password } = await req.body;
+  const newUser = await new UserModel({
+    name,
+    email,
+    town,
+    password,
+    permission: false,
+  });
+  newUser.save();
+  res.json({ message: `Welcome ${newUser.name}`, data: newUser });
+});
