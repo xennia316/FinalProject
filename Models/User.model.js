b47713e1eba7bf3c06e62750d3ec6727c0a24052
xennia316 <xennia316@gmail.com>
2022-05-29 18:01:40 +0100

    User model

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,26 +1,36 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    town: {
+      type: String,
+      required: false,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    permission: {
+      type: Boolean,
+    },
+    wishList: {
+      type: [String],
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  town: {
-    type: String,
-    required: false,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  permission: {
-    type: Boolean,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 
