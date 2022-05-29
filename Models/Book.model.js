d5b2f09048ae9e091205a33d27794d3678ad9859
xennia316 <xennia316@gmail.com>
2022-05-29 18:01:31 +0100

    Book hotel method

--- Models/Book.model.js
+++ Models/Book.model.js
@@ -1,21 +1,30 @@
 const mongoose = require("mongoose");
 
-const BookSchema = new mongoose.Schema({
-  dateIn: {
-    type: Date,
-    required: true,
+const BookSchema = new mongoose.Schema(
+  {
+    dateIn: {
+      type: Date,
+      required: true,
+    },
+    dateOut: {
+      type: Date,
+      required: true,
+    },
+    guests: {
+      type: Number,
+      required: true,
+    },
+    roomsNeeded: {
+      type: Number,
+      required: true,
+    },
+    userId: { type: mongoose.ObjectId, ref: "User" },
+    hotelId: {
+      type: mongoose.ObjectId,
+      ref: "hotels",
+    },
   },
-  dateOut: {
-    type: Date,
-    required: true,
-  },
-  guests: {
-    type: Number,
-    required: true,
-  },
-  roomsNeeded: {
-    type: Number,
-    required: true,
-  },
-  contactDetails: { type: mongoose.ObjectId, ref: "users" },
-});
+  { timestamps: true }
+);
+const Book = mongoose.model("book", BookSchema);
+module.exports = Book;
