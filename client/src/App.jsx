fd6b3dcbfcbc4ce86260b9786de92b191a728d05
xennia316 <xennia316@gmail.com>
2022-05-31 19:44:37 +0100

    signup page test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
 
 const App = () => {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <Signup />
     </>
   );
 };
