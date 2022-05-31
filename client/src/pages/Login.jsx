dc405318d3debac6c5f8a4bc2637abd852652867
xennia316 <xennia316@gmail.com>
2022-05-31 19:45:07 +0100

    login standardized

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,45 +8,54 @@ const Login = () => {
         <section className={`${styles.left}`}>
           <section className={`left-bottom`}>
             <image src="../images/logo.jpg" />
-            <h3>Sign In</h3>
-            <form>
-              <label for="name">
-                <h6>Name:</h6>{" "}
+            <h3 className={styles.h3}>Sign In</h3>
+            <form className={styles.form}>
+              <label for="name" className={styles.label}>
+                <h6 className={styles.h6}>Name:</h6>{" "}
               </label>
               <i class="fa fa-user"></i>
               <input
+                className={styles.input}
                 type={"text"}
                 placeholder="Enter your name"
                 name="name"
                 required
               />
-              <label for="password" className={`text-light`}>
-                <h6>Password:</h6>{" "}
+              <label for="password" className={`text-light ${styles.label}`}>
+                <h6 className={styles.h6}>Password:</h6>{" "}
               </label>
               <input
+                className={styles.input}
                 type={"password"}
                 placeholder="Enter your password"
                 name="password"
                 required
               />
               <div className={`d-flex flex-row mt-3`}>
-                <span className={`m-2 pr-1 text-muted`}>
-                  <a href="#">Forgot password?</a>
+                <span className={`m-2 pr-1 text-muted ${styles.span}`}>
+                  <a href="#" className={styles.a}>
+                    Forgot password?
+                  </a>
                 </span>{" "}
                 <span className={`m-2 pr-1`}>
-                  <a href="#">SignUp instead?</a>
+                  <a href="#" className={styles.a}>
+                    SignUp instead?
+                  </a>
                 </span>
               </div>
 
-              <button type="button" class={`btn-secondary btn p-2 m-5`}>
-                <h5>Sign In</h5>
+              <button
+                type="button"
+                class={`btn-secondary btn p-2 m-5 ${styles.button}`}
+              >
+                <h5 className={styles.h5}>Sign In</h5>
               </button>
             </form>
           </section>
         </section>
         <section className={`${styles.right}`}>
-          <h1>Welcome Back</h1>
-          <h2>Please enter your details</h2>
+          <h1 className={styles.h1}>Welcome Back</h1>
+          <h2 className={styles.h2}>Please enter your details</h2>
         </section>
       </section>
     </section>
