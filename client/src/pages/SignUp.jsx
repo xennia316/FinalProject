dc943df74e887b9490dfe4a7d4140cc42e05b125
xennia316 <xennia316@gmail.com>
2022-05-31 19:45:18 +0100

    signup skeleton

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import styles from "../styles/SignUp.module.css";
+import logo from "../images/logo.jpg";
+
+const SignUp = () => {
+  const inputs = [
+    {
+      label: "Name:",
+      type: "text",
+      value: "name",
+      name: "name",
+      required: true,
+      placeholder: "Please enter your name",
+    },
+    {
+      label: "Email:",
+      type: "email",
+      value: "email",
+      name: "email",
+      required: true,
+      placeholder: "Please enter your email",
+    },
+    {
+      label: "Town:",
+      type: "text",
+      value: "town",
+      name: "town",
+      required: false,
+      placeholder: "Where do you reside",
+    },
+    {
+      label: "Password:",
+      type: "password",
+      value: "password",
+      name: "password",
+      required: true,
+      placeholder: "Your password ...",
+    },
+  ];
+  const ListInput = inputs.map((value, index) => {
+    return (
+      <>
+        <label>
+          <p>{value.label}</p>
+        </label>
+        <input
+          className={styles.input}
+          type={value.type}
+          placeholder={value.placeholder}
+          name={value.name}
+          value={value.value}
+          required={value.required}
+          key={index}
+        />
+      </>
+    );
+  });
+  return (
+    <>
+      <section className={`container-fluid ${styles.body}`}>
+        <section className={`container ${styles.main}`}>
+          <section className={`${styles.left}`}>
+            <section className={`left-bottom`}>
+              <image src={logo} className={`${styles.image}`} />
+              <h3 className={styles.h3}>Sign Up</h3>
+              <form className={styles.forms}>{ListInput}</form>
+            </section>
+            <div className={styles.lineBox}>
+              <div className={styles.line}></div>
+              <p className={styles.p}>OR</p>
+              <div className={styles.line}></div>
+            </div>
+            <section>
+              <div className={`d-flex flex-column `}>
+                <button
+                  className={`btn btn-outline-muted btn-lg col-lg-12 p-2 col-md-8 col-sm-5 text-light  ${styles.sign}`}
+                >
+                  SignUp with google
+                </button>
+                <button
+                  className={`btn btn-warning btn-lg mt-2 col-lg-12 p-2 col-md-8 col-sm-5 text-light ${styles.sign}`}
+                >
+                  SignUp
+                </button>
+                <a href="#" className={styles.a}>
+                  Already have an account?
+                </a>
+              </div>
+            </section>
+          </section>
+          <section className={`${styles.right}`}>
+            <h1 className={styles.h1}>Welcome To Stay</h1>
+            <h2 className={styles.h2}>Please enter your details</h2>
+          </section>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default SignUp;
