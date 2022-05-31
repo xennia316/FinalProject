1d7f4cafb087b216fa8ed553c4fb5e1ea4a61e54
xennia316 <xennia316@gmail.com>
2022-05-31 19:44:55 +0100

    new change

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,13 +1,7 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+import React from "react";
+
+const reportWebVitals = () => {
+  return <div>reportWebVitals</div>;
 };
 
 export default reportWebVitals;
