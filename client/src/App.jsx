88245d1f0cf74ef857f7690aa99730ffb4b230cf
xennia316 <xennia316@gmail.com>
2022-06-01 20:26:01 +0100

    testing components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
+import Card from "./components/Card/Card";
+import SideNav from "./components/sidenav/SideNav";
 
 const App = () => {
   return (
     <>
       {/* <Login /> */}
-      <Signup />
+      {/* <Signup /> */}
+      {/* <Card /> */}
+      <SideNav />
     </>
   );
 };
