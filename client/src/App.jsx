8a2c71f061ef91e6dcfa0ba902e01bc83ff503c8
xennia316 <xennia316@gmail.com>
2022-06-02 20:42:39 +0100

    new component tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
 import Card from "./components/Card/Card";
 import SideNav from "./components/sidenav/SideNav";
+import Home from "./pages/Home";
+import Wish from "./components/WishCard/WishCard";
 
 const App = () => {
   return (
@@ -10,7 +12,9 @@ const App = () => {
       {/* <Login /> */}
       {/* <Signup /> */}
       {/* <Card /> */}
-      <SideNav />
+      {/* <SideNav /> */}
+      <Home />
+      {/* <Wish /> */}
     </>
   );
 };
