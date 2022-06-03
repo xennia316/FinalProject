e28d2321646ffe91a69ab76c405b8bab5227cd44
xennia316 <xennia316@gmail.com>
2022-06-03 22:28:31 +0100

    routes and context switching added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
 import Card from "./components/Card/Card";
 import SideNav from "./components/sidenav/SideNav";
 import Home from "./pages/Home";
 import Wish from "./components/WishCard/WishCard";
+import FullCard from "./components/FullCard/FullCard";
+import HotelProvider from "./HotelContext";
+import BookHotel from "./components/FullCard/BookHotel";
+import WishList from "./components/WishList/wishList";
 
 const App = () => {
   return (
-    <>
-      {/* <Login /> */}
-      {/* <Signup /> */}
-      {/* <Card /> */}
-      {/* <SideNav /> */}
-      <Home />
-      {/* <Wish /> */}
-    </>
+    <HotelProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/signup" element={<Signup></Signup>}></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/wishlist" element={<WishList></WishList>}></Route>
+        </Routes>
+      </Router>
+    </HotelProvider>
   );
 };
 
