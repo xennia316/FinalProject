80df7335bb17034cb0e82dedd32b957a3062db30
xennia316 <xennia316@gmail.com>
2022-06-04 20:35:16 +0100

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,28 +13,71 @@ import Logo from "../images/side-logo.png";
 import BookHotel from "../components/FullCard/BookHotel";
 import axios from "axios";
 
+import mountainhotel from "../images/mountain-hotel.jpg";
+
 const cards = [
-  { name: "Hotel Semme", location: "Limbe" },
-  { name: "Eta palace", location: "Buea" },
-  { name: "Hotel Semme", location: "Limbe" },
-  { name: "Eta palace", location: "Buea" },
-  { name: "Hotel Semme", location: "Limbe" },
-  { name: "Eta palace", location: "Buea" },
-  { name: "Hotel Semme", location: "Limbe" },
-  { name: "Eta palace", location: "Buea" },
+  {
+    name: "Hotel Semme",
+    location: "Limbe",
+    imageurl:
+      "http://searchcameroon.com/wp-content/uploads/2021/06/hilton-hotel-yaounde-SC1-580x408.jpg",
+  },
+  // { name: "Bingo", location: "Buea, imageurl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.afriquephotos.net%2Fen%2Fmedia%2F043fe559-1f21-4345-a8e6-cc79a8bfdf87-mount-febe-hotel-yaounde-cameroon&psig=AOvVaw2qFTIAK-IF39xjOhWaS1Qf&ust=1654421030307000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJjhqOG8k_gCFQAAAAAdAAAAABBI"},
+  {
+    name: "Hotel Semme",
+    location: "Douala",
+    imageurl:
+      "https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2021/07/hilton-hotel-yaounde.jpg",
+  },
+  {
+    name: "El palacio",
+    location: "Kribi",
+    imageurl:
+      "https://blog.staah.com/wp-content/uploads/2018/03/hotelsign-min.jpg",
+  },
+  {
+    name: "Chariot",
+    location: "Edea",
+    imageurl:
+      "https://image.shutterstock.com/z/stock-photo-luxury-hotel-with-infinity-pool-and-palms-all-around-it-648165631.jpg",
+  },
+  {
+    name: "WDC",
+    location: "Buea",
+    imageurl:
+      "https://static.wixstatic.com/media/8dc5c1_d4939e195c5549ae9fc927bbb40dc368~mv2.jpg/v1/fill/w_1080,h_720,al_c,q_85,enc_auto/8dc5c1_d4939e195c5549ae9fc927bbb40dc368~mv2.jpg",
+  },
+  {
+    name: "Hilton",
+    location: "Yaounde",
+    imageurl:
+      "https://res.cloudinary.com/estate-intel/images/f_auto,q_auto/v1620432644/wp_assets/Radisson_Blu_Hotel__Apartments_Douala_Cameroon-700x868/Radisson_Blu_Hotel__Apartments_Douala_Cameroon-700x868.jpg?_i=AA",
+  },
+  {
+    name: "Mountain hotel",
+    location: "Bafoussam",
+    imageurl: mountainhotel,
+  },
 ];
 
 const cardList = cards.map((card, index) => {
-  return <Card name={card.name} location={card.location} key={index} />;
+  return (
+    <Card
+      name={card.name}
+      location={card.location}
+      imageurl={card.imageurl}
+      key={index}
+    />
+  );
 });
 
 const wishcards = [
   { name: "Mountain hotel", location: "Buea", price: "2.5k" },
-  { name: "Mountain hotel", location: "Limbe", price: "3.5k" },
-  { name: "Mountain hotel", location: "Douala", price: "4.5k" },
+  { name: "Chariot", location: "Limbe", price: "3.5k" },
+  { name: "El palacio", location: "Douala", price: "4.5k" },
   { name: "Mountain hotel", location: "Dubai", price: "6.5k" },
   { name: "DaCosta ", location: "Here", price: "7.5k" },
-  { name: "Mountain hotel", location: "There", price: "8.5k" },
+  { name: "Brigeton", location: "There", price: "8.5k" },
 ];
 
 const listWishCards = wishcards.map((cards, index) => {
@@ -44,6 +87,7 @@ const listWishCards = wishcards.map((cards, index) => {
       name={cards.name}
       location={cards.location}
       price={cards.price}
+      image={cards.imageurl}
       key={index}
     />
   );
