97aaa7eb7c68ca10e9c4d49390eca802e2f4630d
xennia316 <xennia316@gmail.com>
2022-06-04 20:34:32 +0100

    wish card styles

--- client/src/components/WishList/wishList.jsx
+++ client/src/components/WishList/wishList.jsx
@@ -1,15 +1,30 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useContext } from "react";
 
 import styles from "./wishlist.module.css";
 
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 import WishCard from "../WishCard/WishCard";
+import picture from "../../images/hotel2.jpeg";
+import { HotelContext } from "../../HotelContext";
 import FullCard from "../FullCard/FullCard";
 import SideNav from "../sidenav/SideNav";
 
-const wishList = () => {
+const WishList = (param) => {
+  const { setData } = useContext(HotelContext);
+
+  const handleClick = () => {
+    const data = {
+      image: picture,
+      name: param.name,
+      location: param.location,
+      price: param.price,
+    };
+
+    setData(data);
+  };
+
   return (
     <section className={styles.main}>
       <section className={styles.sideNav}>
@@ -18,7 +33,7 @@ const wishList = () => {
       <section className={`container p-5 ${styles.body}`}>
         <h3 className={`text-info`}>Your Wish List</h3>
         <section className={styles.wishList}>
-          <WishCard />
+          <WishCard onClick={handleClick} />
           <section className={styles.side}>
             <FontAwesomeIcon icon={faTrashAlt} />
             <button className={` btn btn-warning text-light ${styles.button}`}>
@@ -27,7 +42,7 @@ const wishList = () => {
           </section>
         </section>
         <section className={styles.wishList}>
-          <WishCard />
+          <WishCard className={styles.real} />
           <section className={styles.side}>
             <FontAwesomeIcon icon={faTrashAlt} />
             <button className={` btn btn-warning text-light ${styles.button}`}>
@@ -90,7 +105,8 @@ const wishList = () => {
           </section>
         </section>
       </section>
-      <section className={styles.righContainer}>
+      <section className={styles.rightContainer}>
+        <h3>Hotel Details</h3>
         <section className={styles.right}>
           <FullCard />
         </section>
@@ -99,4 +115,4 @@ const wishList = () => {
   );
 };
 
-export default wishList;
+export default WishList;
