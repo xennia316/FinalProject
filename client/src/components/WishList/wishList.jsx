c1c841017082e594fc64d6c5f7f20733eb287a8d
xennia316 <xennia316@gmail.com>
2022-06-05 21:04:40 +0100

    wishlist construction

--- client/src/components/WishList/wishList.jsx
+++ client/src/components/WishList/wishList.jsx
@@ -1,7 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faSearch } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import SideNav from "../sidenav/SideNav";
+import styles from "./wishlist.module.css";
 
 const wishList = () => {
-  return <div>wishList</div>;
+  return (
+    <section className={`d-flex ${styles.all}`}>
+      <section>
+        <SideNav />
+      </section>
+      <section className={`${styles.top}`}>
+        <section className={`d-flex justify-space-between ${styles.heading}`}>
+          <FontAwesomeIcon icon={faArrowLeft} />
+          <h4>WishList</h4>
+          <FontAwesomeIcon icon={faSearch} />
+        </section>
+        <section>
+          <img />
+        </section>
+      </section>
+    </section>
+  );
 };
 
 export default wishList;
