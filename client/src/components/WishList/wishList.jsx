1560eedeaaa8c85b1ea7d251589bce0e2ed2df2d
xennia316 <xennia316@gmail.com>
2022-06-07 22:18:40 +0100

    structuring card

--- client/src/components/WishList/wishList.jsx
+++ client/src/components/WishList/wishList.jsx
@@ -1,8 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faSearch,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import SideNav from "../sidenav/SideNav";
 import styles from "./wishlist.module.css";
+import image from "../../images/gradient.png";
 
 const wishList = () => {
   return (
@@ -16,8 +21,27 @@ const wishList = () => {
           <h4>WishList</h4>
           <FontAwesomeIcon icon={faSearch} />
         </section>
-        <section>
-          <img />
+        <section className={`d-flex ${styles.subb}`}>
+          <img src={image} className={styles.image} alt="" />
+          <section className={`d-flex ${styles.sub}`}>
+            <section className={styles.sub1}>
+              <section>
+                <p>Name</p>
+                <p>Location</p>
+              </section>
+              <section>
+                <p>Price</p>
+              </section>
+            </section>
+            <section className={`d-flex  flex-column`}>
+              <section>
+                <FontAwesomeIcon icon={faTrashCan} />
+              </section>
+              <section>
+                <button className={`btn text-light btn-info`}>Book</button>
+              </section>
+            </section>
+          </section>
         </section>
       </section>
     </section>
