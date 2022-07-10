6fad6f1ca4a0396d1f106c5e76d4b1479ff69d80
xennia316 <xennia316@gmail.com>
2022-07-10 11:36:28 +0100

    minor change

--- client/src/components/FullCard/FullCard.jsx
+++ client/src/components/FullCard/FullCard.jsx
@@ -1,14 +1,69 @@
-import React, { useContext } from "react";
+import React, { useContext, useId, useState } from "react";
 
 import styles from "./FullCard.module.css";
 
-import mainPic from "../../images/gradient4.jpg";
+import axios from "axios";
 import sidePic from "../../images/hotel-back.webp";
 import sidePic2 from "../../images/hotel2.jpeg";
 import { HotelContext } from "../../HotelContext";
 import BookHotel from "./BookHotel";
+import { useNavigate } from "react-router-dom";
 
 const FullCard = (param) => {
+  const today = Date.now();
+  const navigate = useNavigate();
+
+  const [booking, setBooking] = useState({
+    dateIn: "",
+    dateOut: "",
+    guests: 0,
+    roomsNeeded: 0,
+    userId: localStorage.getItem("_id"),
+  });
+
+  const handleBooking = (e) => {
+    const { name, value } = e.target;
+    setBooking({
+      ...booking,
+      [name]: value,
+    });
+  };
+
+  const checkBooking = (booking) => {
+    if (
+      booking.dateIn < today ||
+      booking.dateOut < today ||
+      booking.guests < 0 ||
+      booking.roomsNeeded < 0
+    ) {
+      alert("Invalid operations");
+      setBooking({
+        dateIn: "",
+        dateOut: "",
+        guests: 0,
+        roomsNeeded: 0,
+        userId: localStorage.getItem("_id"),
+      });
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (checkBooking(booking)) {
+      let result = await axios
+        .post("/api/book/book-hotel", booking)
+        .then((data) => data);
+      if (result.status === 200) {
+        localStorage.setItem("bookedHotel", JSON.stringify(result.data.data));
+        navigate("/home");
+      } else {
+        alert("doesn't work!");
+      }
+    }
+  };
+
   const { data, setShowModal } = useContext(HotelContext);
 
   const handleBook = () => {
